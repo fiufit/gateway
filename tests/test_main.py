c8dc345c6ac015e1e3f05c0d14cc6b8548299021
TGRodriguez <tgrodriguez@fi.uba.ar>
2023-04-02 23:58:58 -0300

    Modified tests to add versions in each path

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,14 +8,14 @@ client = TestClient(main.app)
 @patch("src.main.make_request") # OJO en el patch hay que usar el path entero
 def test_successful_register(mock_make_request):
     mock_make_request.return_value = {"success": True}
-    response = client.post("/users/register", json={"email": "test@example.com", "password": "testpassword"})
+    response = client.post("/v1/users/register", json={"email": "test@example.com", "password": "testpassword"})
     assert response.status_code == 200
     assert response.json() == {"success": True}
 
 
 
 def test_incorrect_email_format_in_body_register():
-    response = client.post("/users/register", json={"e-mail": "test@example.com", "password": "testpassword"})
+    response = client.post("/v1/users/register", json={"e-mail": "test@example.com", "password": "testpassword"})
     assert response.status_code == 400
     response_body = response.json()
     assert response_body['error']['code'] == 'CODIGO_LOCO'
@@ -24,7 +24,7 @@ def test_incorrect_email_format_in_body_register():
 
 
 def test_incorrect_password_format_in_body_register():
-    response = client.post("/users/register", json={"email": "test@example.com", "pw": "testpassword"})
+    response = client.post("/v1/users/register", json={"email": "test@example.com", "pw": "testpassword"})
     assert response.status_code == 400
     response_body = response.json()
     assert response_body['error']['code'] == 'CODIGO_LOCO'
@@ -33,7 +33,7 @@ def test_incorrect_password_format_in_body_register():
 
 
 def test_incorrect_email_and_password_format_in_body_register():
-    response = client.post("/users/register", json={"e-mail": "test@example.com", "pw": "testpassword"})
+    response = client.post("/v1/users/register", json={"e-mail": "test@example.com", "pw": "testpassword"})
     assert response.status_code == 400
     response_body = response.json()
     assert response_body['error']['code'] == 'CODIGO_LOCO'
@@ -43,7 +43,7 @@ def test_incorrect_email_and_password_format_in_body_register():
 
 def test_finish_register_without_authorization_header():
     headers = {"NotAuth": "Bearer fake_token"}
-    response = client.post("/users/finish_register", json={"name": "Test User"}, headers=headers)
+    response = client.post("/v1/users/finish-register", json={"name": "Test User"}, headers=headers)
     assert response.status_code == 401
     response_body = response.json()
     assert response_body['error']['code'] == 'CODIGO_LOCO'
@@ -53,7 +53,7 @@ def test_finish_register_without_authorization_header():
 
 def test_finish_register_without_bearer_keyword_in_header():
     headers = {"Authorization": "fake_token"}
-    response = client.post("/users/finish_register", json={"name": "Test User"}, headers=headers)
+    response = client.post("/v1/users/finish-register", json={"name": "Test User"}, headers=headers)
     assert response.status_code == 401
     response_body = response.json()
     assert response_body['error']['code'] == 'CODIGO_LOCO'
@@ -63,7 +63,7 @@ def test_finish_register_without_bearer_keyword_in_header():
 
 def test_finish_register_with_other_keyword_in_header():
     headers = {"Authorization": "NotBearer fake_token"}
-    response = client.post("/users/finish_register", json={"name": "Test User"}, headers=headers)
+    response = client.post("/v1/users/finish-register", json={"name": "Test User"}, headers=headers)
     assert response.status_code == 401
     response_body = response.json()
     assert response_body['error']['code'] == 'CODIGO_LOCO'
@@ -77,8 +77,8 @@ def test_successful_finish_register(mock_get_validated_user, mock_make_request):
     mock_get_validated_user.return_value = {"name":"Lionel Messi", "uid":"fake_uid"}
     headers = {"Authorization": "Bearer fake_token"}
     body = {
-        "nickname": "LioMessi",
-        "displayname": "Lionel Messi",
+        "nick_name": "LioMessi",
+        "display_name": "Lionel Messi",
         "is_male": True,
         "birth_date": "18/12/2022",
         "height": 169,
@@ -91,7 +91,7 @@ def test_successful_finish_register(mock_get_validated_user, mock_make_request):
             "la del mundo",
         ]
     }
-    response = client.post("/users/finish_register", json=body, headers=headers)
+    response = client.post("/v1/users/finish-register", json=body, headers=headers)
     assert response.status_code == 200
     assert response.json() == {"success": True}
 
