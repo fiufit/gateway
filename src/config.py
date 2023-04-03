2c4037d51777ce77fbfd47a36053812d55fa3b7f
TGRodriguez <tgrodriguez@fi.uba.ar>
2023-04-02 23:57:20 -0300

    Changed firebase and needs auth variables

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ REGISTER_PATH = os.getenv("REGISTER_PATH", "localhost")
 FINISH_REGISTER_PATH = os.getenv("FINISH_REGISTER_PATH", "localhost")
 APP_HOST = os.getenv("HOST", "localhost")
 APP_PORT = int(os.getenv("PORT", "8000"))
-FIREBASE_ADMIN = os.getenv("FIREBASE_SDK_JSON_PATH", "firebase.json")
+FIREBASE_ADMIN = os.getenv("FIREBASE_ADMIN", "firebase.json")
 
 #Endpoints que necesitan validar jwt y mail verificado
-NEEDS_AUTH = ["/users/finish_register"]
+NEEDS_AUTH = ["users/finish-register"]
