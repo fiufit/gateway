011bcdb995aada5ef0f6a185ff7797c8baa20b08
TGRodriguez <tgrodriguez@fi.uba.ar>
2023-06-23 00:32:09 -0300

    added get and update endpoint for push notifications

---
+++ src/models/notifications/get_push.py
@@ -0,0 +1,17 @@
+from typing import Optional
+from pydantic import BaseModel
+
+
+class GetNotificationsRequest(BaseModel):
+    read: bool
+    limit: Optional[int]
+    next_cursor: Optional[str]
+
+    def to_query_string(self):
+        params = self.dict()
+        query = []
+        for param, value in params.items():
+            if value is None:
+                continue
+            query.append(f"{param}={str(value).lower()}")
+        return "&".join(query)
--- src/routers/notifications.py
+++ src/routers/notifications.py
@@ -12,6 +12,8 @@ from auth.user_jwt_bearer import (
 )
 from auth.jwt_bearer import JWTBearer
 from models.notifications.create_push import CreateNotificationRequest
+from models.notifications.update_push import UpdateNotificationRequest
+from models.notifications.get_push import GetNotificationsRequest
 from models.notifications.create_subscriber import CreateSubscriberRequest
 from models.notifications.update_subscriber import UpdateSubscriberRequest
 
@@ -70,4 +72,39 @@ async def update_subscriber(
         dict(request.headers),
         request.method,
         {"user_id": uid, **request_model.dict()},
+    )
+
+
+@router.get("/{version}/notifications/push", tags=["notifications"])
+async def get_push_notifications(
+    request: Request,
+    version,
+    request_model: GetNotificationsRequest = Depends(),
+    user: dict = Depends(user_auth_scheme),
+):
+    uid = user["uid"]
+    params = request_model.to_query_string()
+    url = f"{NOTIFICATIONS_URL}/api/{version}/notifications/push?{params}&user_id={uid}"
+    return await make_request(
+        url,
+        dict(request.headers),
+        request.method,
+        {},
+    )
+
+
+@router.patch("/{version}/notifications/push/{id}", tags=["notifications"])
+async def update_push_notification(
+    request: Request,
+    request_model: UpdateNotificationRequest,
+    version,
+    id,
+    _: dict = Depends(auth_scheme),
+):
+    url = NOTIFICATIONS_URL + "/api/" + version + "/notifications/push/" + id
+    return await make_request(
+        url,
+        dict(request.headers),
+        request.method,
+        {**request_model.dict()},
     )
